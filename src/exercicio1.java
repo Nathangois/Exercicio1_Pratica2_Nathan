5dd743a0ad76ec8fbf6f6d861aa5244552ab7526
Nathangois <eunathangois@gmail.com>
2023-04-10 22:37:26 -0300

    commit

--- src/exercicio1.java
+++ src/exercicio1.java
@@ -4,11 +4,23 @@
 trocar figurinhas com o colega, mas cada um faz o seu.
 * Data: 10/04/2023
 */
+import java.util.Scanner;
 public class exercicio1 {
     public static void main(String[] args) throws Exception {
 
+        double NumeroA, NumeroB;
 
-        
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Me informe número A");
+        NumeroA = teclado.nextDouble();
+        System.out.println("Me informe número B");
+        NumeroB = teclado.nextDouble();
+        teclado.close();
+        if (NumeroA > NumeroB) {
+            System.out.print("O número maior é " + NumeroA);
+        }
+        if (NumeroA < NumeroB) {
+            System.out.print("O número maior é " + NumeroB);
+        }
     }
 }
