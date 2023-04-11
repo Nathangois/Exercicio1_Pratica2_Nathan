0d4def32ff2139e9ff24e9ab620a0ed5a7aea2a9
Nathangois <eunathangois@gmail.com>
2023-04-10 22:23:45 -0300

    primeiraremessa

---
+++ src/exercicio1.java
@@ -0,0 +1,14 @@
+/* Nome do Aluno: Nathan Almeida Gois
+* RA: 722311425
+* Nome do Programa: . A prática deve ser feita individualmente, pode conversar,
+trocar figurinhas com o colega, mas cada um faz o seu.
+* Data: 10/04/2023
+*/
+public class exercicio1 {
+    public static void main(String[] args) throws Exception {
+
+
+        
+        System.out.println("Hello, World!");
+    }
+}
